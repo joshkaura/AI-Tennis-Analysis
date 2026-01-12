e2883d355a0c31f1f7ae1a1453c00b1bb946d691
joshkaura <josh.kaura02@gmail.com>
2026-01-12 00:03:13 +0000

    yolo inference tracking

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
+'''
+
+# General Models:
 model = YOLO('yolo11x.pt')
 model_fast = YOLO('yolo11n.pt')
 
-fast = False
+fast = True
 
 if fast == True:
     result = model_fast.predict('input_videos/input_video.mp4', save=True)
@@ -13,7 +16,27 @@ else:
 print(result)
 
 '''
+
+'''
 print("Boxes: ")
 for box in result[0].boxes:
     print(box)
 '''
+
+'''
+#trained - tennis ball
+
+model = YOLO('models/tennis_ball_yolo11n_last.pt')
+
+result = model.predict('input_videos/input_video.mp4', conf = 0.2, save=True)
+
+print(result)
+
+'''
+
+# Track Players
+model = YOLO('yolo11x.pt')
+
+result = model.track('input_videos/input_video.mp4', device = "mps", conf = 0.2, save=True)
+
+print(result)
