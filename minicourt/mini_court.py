7e244ecbc510405daa55d7c7204161b469e45c81
joshkaura <josh.kaura02@gmail.com>
2026-01-14 16:46:41 +0000

    mini court changes

--- minicourt/mini_court.py
+++ minicourt/mini_court.py
@@ -195,8 +195,65 @@ class MiniCourt():
                                         )
 
         return  mini_court_player_position
+    
+    def convert_bounding_boxes_to_mini_court_coordinates(self,player_boxes, ball_boxes, original_court_key_points ):
+        player_heights = {
+            1: constants.PLAYER_1_HEIGHT_METERS,
+            2: constants.PLAYER_2_HEIGHT_METERS
+        }
 
-    def convert_bounding_boxes_to_mini_court_coordinates(self, player_boxes, ball_boxes, original_court_key_points):
+        output_player_boxes= []
+        output_ball_boxes= []
+
+        for frame_num, player_bbox in enumerate(player_boxes):
+            ball_box = ball_boxes[frame_num][1]
+            ball_position = get_centre_bbox(ball_box)
+
+            if player_bbox is not None and ball_position is not None:
+                closest_player_id_to_ball = min(player_bbox.keys(), key=lambda x: measure_distance(ball_position, get_centre_bbox(player_bbox[x])))
+
+            output_player_bboxes_dict = {}
+            for player_id, bbox in player_bbox.items():
+                foot_position = get_foot_position(bbox)
+
+                # Get The closest keypoint in pixels
+                closest_key_point_index = get_closest_keypoint_index(foot_position,original_court_key_points, [0,2,12,13])
+                closest_key_point = (original_court_key_points[closest_key_point_index*2], 
+                                     original_court_key_points[closest_key_point_index*2+1])
+
+                # Get Player height in pixels
+                frame_index_min = max(0, frame_num-20)
+                frame_index_max = min(len(player_boxes), frame_num+50)
+                bboxes_heights_in_pixels = [get_height_of_bbox(player_boxes[i][player_id]) for i in range (frame_index_min,frame_index_max)]
+                max_player_height_in_pixels = max(bboxes_heights_in_pixels)
+
+                mini_court_player_position = self.get_mini_court_coordinates(foot_position,
+                                                                            closest_key_point, 
+                                                                            closest_key_point_index, 
+                                                                            max_player_height_in_pixels,
+                                                                            player_heights[player_id]
+                                                                            )
+                
+                output_player_bboxes_dict[player_id] = mini_court_player_position
+
+                if closest_player_id_to_ball == player_id:
+                    # Get The closest keypoint in pixels
+                    closest_key_point_index = get_closest_keypoint_index(ball_position,original_court_key_points, [0,2,12,13])
+                    closest_key_point = (original_court_key_points[closest_key_point_index*2], 
+                                        original_court_key_points[closest_key_point_index*2+1])
+                    
+                    mini_court_player_position = self.get_mini_court_coordinates(ball_position,
+                                                                            closest_key_point, 
+                                                                            closest_key_point_index, 
+                                                                            max_player_height_in_pixels,
+                                                                            player_heights[player_id]
+                                                                            )
+                    output_ball_boxes.append({1:mini_court_player_position})
+            output_player_boxes.append(output_player_bboxes_dict)
+
+        return output_player_boxes , output_ball_boxes
+
+    def convert_bounding_boxes_to_mini_court_coordinates_2(self, player_boxes, ball_boxes, original_court_key_points):
         player_heights = {
             1: constants.PLAYER_1_HEIGHT_METERS,
             2: constants.PLAYER_2_HEIGHT_METERS
